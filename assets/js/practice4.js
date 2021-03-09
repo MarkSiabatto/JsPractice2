fb9c938624935db59c787a828e88bd448a16708a
MarcoSiabatto <siabattorodriguezmarco@gmail.com>
2021-03-08 21:09:16 -0500

    Updated exercise with correct Prettier formatting.

--- assets/js/practice4.js
+++ assets/js/practice4.js
@@ -1,44 +1,44 @@
 // Variables
 
-let enterInfo=document.getElementById("enterInfo");
-let name1=document.getElementById("name1");
-let age=document.getElementById("age");
-let docId=document.getElementById("docId");
-let phoneNum=document.getElementById("phoneNum");
+let enterInfo = document.getElementById("enterInfo");
+let name1 = document.getElementById("name1");
+let age = document.getElementById("age");
+let docId = document.getElementById("docId");
+let phoneNum = document.getElementById("phoneNum");
 
 // Function
-const saveInfo=()=>{
-    let saveName=prompt("Please Enter Your Name: ")
+const saveInfo = () => {
+    let saveName = prompt("Please Enter Your Name: ");
 
     if (saveName === "") {
-        alert("Please input your name before continuing!");
+    alert("Please input your name before continuing!");
     } else {
-        name1.innerHTML= "Name: "+ saveName;
+    name1.innerHTML = "Name: " + saveName;
     }
-    let saveAge=prompt("Please Enter Your Age: ")
+    let saveAge = prompt("Please Enter Your Age: ");
 
     if (saveAge === "") {
-        alert("Please input your age before continuing!");
+    alert("Please input your age before continuing!");
     } else {
-        age.innerHTML="Age: "+ saveAge;
+    age.innerHTML = "Age: " + saveAge;
     }
-    let saveDoc=prompt("Please Enter Your Document Number: ")
+    let saveDoc = prompt("Please Enter Your Document Number: ");
 
     if (saveDoc === "") {
-        alert("Please input your document number before continuing!")
+    alert("Please input your document number before continuing!");
     } else {
-        docId.innerHTML="Document Id: "+saveDoc;
+    docId.innerHTML = "Document Id: " + saveDoc;
     }
-    let savePhone=prompt("Please Enter Your Phone Number: ")
+    let savePhone = prompt("Please Enter Your Phone Number: ");
 
     if (savePhone === "") {
-        alert("Please enter your phone number before continuing!")
+    alert("Please enter your phone number before continuing!");
     } else {
-        phoneNum.innerHTML="Phone Number: "+savePhone;
+    phoneNum.innerHTML = "Phone Number: " + savePhone;
     }
-}
+};
 
 // OnClicks
-enterInfo.onclick=function (){
-    saveInfo();
-}
+enterInfo.onclick = function () {
+  saveInfo();
+};
